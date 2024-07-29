7a1c5986f5f340135719236f7bbc5da462565265
khoa3107 <154038318+khoa3107@users.noreply.github.com>
2024-07-29 21:23:24 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = '💘';
+const giftUrl = '';
+const eventName = 'Chào chị iu';
+const titleCard = 'Em Tặng Chị';
+const contentCard = 'Em thích chị,chị Quyên ';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
